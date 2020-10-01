28d04436159ba0f306ac282ab1f051ec96c25859
KirillAsmanov <mazusik1@gmail.com>
2020-10-02 02:49:25 +0300

    Init web application

---
+++ src/main/java/ru/asmanov/quizzes/QuizzesApplication.java
@@ -0,0 +1,13 @@
+package ru.asmanov.quizzes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzesApplication.class, args);
+    }
+
+}
